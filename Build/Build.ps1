c80dd06e6e7a57f5746eaa8927162ba546912b90
Neil White <Neil.White@cnr.com>
2023-10-10 15:13:32 -0700

    Modified build script

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -4,18 +4,20 @@
 #>
 [CmdletBinding()]
 param ()
+$Script:ErrorActionPreference = 'Stop'
 $Separator = [System.IO.Path]::DirectorySeparatorChar
 Write-Information "Put all the module files' content into a single file"
-$RootModulePath = "$PSScriptRoot$Separator`NexusIQ.psm1"
-Set-Content -Path $RootModulePath -Value (Get-Content "$PSScriptRoot$Separator`Base.psm1")
-Remove-Item "$PSScriptRoot$Separator`Base.psm1"
+$SourceDir = Split-Path $PSScriptRoot
+$RootModulePath = "$SourceDir$Separator`NexusIQ.psm1"
+Set-Content -Path $RootModulePath -Value (Get-Content "$SourceDir$Separator`Base.psm1")
+Remove-Item "$SourceDir$Separator`Base.psm1"
 
 Write-Information "Add the rest of the file content"
-$ModuleFiles = Get-ChildItem -File -Path $PSScriptRoot -Filter "*.psm1" -Exclude ([System.IO.Path]::GetFileName($RootModulePath))
+$ModuleFiles = Get-ChildItem -File -Path $SourceDir -Filter "*.psm1" | Where-Object -Property Name -NE ([System.IO.Path]::GetFileName($RootModulePath))
 ($ModuleFiles | Get-Content) -replace "using\smodule\s\.\\Base\.psm1","" | Add-Content -Path $RootModulePath
 
 Write-Information "Modify the module manifest to include only a single module file."
-$ManifestFile = "$PSScriptRoot$Separator`NexusIQ.psd1"
+$ManifestFile = "$SourceDir$Separator`NexusIQ.psd1"
 (Get-Content $ManifestFile) -replace "NestedModules\s\=\s@\(.*\)","NestedModules = @()" |
 Set-Content -Path $ManifestFile
 
