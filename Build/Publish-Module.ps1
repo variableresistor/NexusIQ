87b24d91e8a05b53124fa7311647bcea32e123c9
Neil White <variableresistor@gmail.com>
2024-01-12 16:01:11 -0800

    Module publish script is a more robust

--- Build/Publish-Module.ps1
+++ Build/Publish-Module.ps1
@@ -1,13 +1,24 @@
-(Get-Content "./$env:ModuleName.psd1") -replace "ModuleVersion = '.*'","ModuleVersion = '$env:Version'" |
-Set-Content -Path "./$env:ModuleName.psd1" -PassThru
-if (Get-Module -Name $env:ModuleName -ListAvailable)
-{
-  Remove-Module -Name $env:ModuleName -ErrorAction SilentlyContinue
-  Uninstall-Module -Name $env:ModuleName -AllVersion -Verbose
-}
-$env:ModulePath += ";$env:Pipeline_Workspace"
-Get-ChildItem -Directory | Remove-Item -Recurse -Verbose
-Import-Module $env:ModuleName -Verbose -RequiredVersion $env:Version
+[CmdletBinding()]
+param (
+  [ValidateNotNullOrEmpty()][string]$NuGetApiKey = $env:NuGetApiKey
+)
+$Script:ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 1 # Just to be extra careful
 
-# Publish-Module -Name $env:ModuleName -Repository PSGallery -NuGetApiKey $(NuGetApiKey) -Verbose
+$ModuleFolderPath = (Split-Path $PSScriptRoot)
+$ModuleName = [System.IO.Path]::GetFileName($ModuleFolderPath)
+$Version = ((Get-Content "$ModuleFolderPath\azure-pipelines.yml" | Select-String -Pattern "name:\s\d" -Raw) -replace "name: ","").Trim()
+(Get-Content "$ModuleFolderPath\$ModuleName.psd1") -replace "ModuleVersion = '.*'","ModuleVersion = '$Version'" |
+Set-Content -Path "$ModuleFolderPath\$ModuleName.psd1" -PassThru
+
+if (Get-Module -Name $ModuleName -ListAvailable)
+{
+  Remove-Module -Name $ModuleName -ErrorAction SilentlyContinue
+  Uninstall-Module -Name $ModuleName -AllVersion -Verbose
+}
+$env:PSModulePath += ";$(Split-Path $ModuleFolderPath)"
+Get-ChildItem -Directory | Remove-Item -Recurse -Verbose
+Import-Module $ModuleName -Verbose -RequiredVersion $Version
+
+# Publish-Module -Name $env:ModuleName -Repository PSGallery -NuGetApiKey $NuGetApiKey -Verbose
 git reset --hard
