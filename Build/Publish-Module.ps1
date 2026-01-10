bda9e9ee25d90956aea3adb81ad8b5016dfb848d
Neil White <variableresistor@gmail.com>
2026-01-10 01:25:51 -0800

    Added TSing code

--- Build/Publish-Module.ps1
+++ Build/Publish-Module.ps1
@@ -22,5 +22,10 @@ if (Get-Module -Name $ModuleName -ListAvailable)
 Resolve-Path "$TempDirectory/$ModuleName" | Import-Module -Verbose
 $ModuleInfo = Get-Module -Name $ModuleName
 $ModuleInfo
-Get-ChildItem (Split-Path $ModuleInfo) -Recurse
+"----Temp Directory-----"
+Get-ChildItem $TempDirectory | Select-Object -ExpandProperty Path
+"----Module Directory-----"
+Get-ChildItem "$TempDirectory/$ModuleName" | Select-Object -ExpandProperty Path
+"----Imported Module Directory-----"
+Get-ChildItem (Split-Path $ModuleInfo) -Recurse | Select-Object -ExpandProperty Path
 # Publish-Module -Name $ModuleName -Repository PSGallery -NuGetApiKey $env:NuGetApiKey -Verbose
