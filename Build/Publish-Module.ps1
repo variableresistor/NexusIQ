adac884114258e071e15895f9bea5df4b7ecbf6d
Neil White <variableresistor@gmail.com>
2026-01-10 01:42:45 -0800

    Test

--- Build/Publish-Module.ps1
+++ Build/Publish-Module.ps1
@@ -9,7 +9,11 @@ param (
 
   [Parameter()]
   [ValidateScript({Test-Path $_})]
-  [string]$TempDirectory = $env:AGENT_TEMPDIRECTORY
+  [string]$TempDirectory = $env:AGENT_TEMPDIRECTORY,
+
+  [Parameter()]
+  [ValidateScript({Test-Path $_})]
+  [string]$SourcesDirectory = $env:AGENT_TEMPDIRECTORY
 )
 $Script:ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 1 # Just to be extra careful
@@ -24,12 +28,13 @@ $Divider = switch (Test-Path Env:\Path)
   $true { ";" } # Windows
   $false { ":" } # Linux
 }
+Set-Location $TempDirectory
 $env:PSModulePath += "$Divider$TempDirectory"
-$env:PSModulePath
-$env:PSModulePath.Split($Divider)
+Get-ChildItem -Path $SourcesDirectory | Remove-Item -Verbose -Recurse
 Resolve-Path "$TempDirectory/$ModuleName" | Import-Module -Verbose
 $ModuleInfo = Get-Module -Name $ModuleName
 $ModuleInfo
 "----Imported Module Directory-----"
 Get-ChildItem (Split-Path $ModuleInfo) -Recurse | Select-Object -ExpandProperty FullName
 # Publish-Module -Name $ModuleName -Repository PSGallery -NuGetApiKey $env:NuGetApiKey -Verbose
+git -C $SourcesDirectory reset --hard
